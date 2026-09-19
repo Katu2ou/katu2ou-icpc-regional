7a9cec86a826368eaedfcd454ea014790eeefdf5
ciesle <ciesle.dev@gmail.com>
2026-09-20 00:53:04 +0900

    make codes shorten

--- hld_short.cpp
+++ hld_short.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 vector<vector<int>> g;
 vector<int> sz, in, out, nxt;
 int t;
-
 void dfs_sz(int v = 0) {
     sz[v] = 1;
     for(auto &u: g[v]) {
-        dfs_sz(u);
-        sz[v] += sz[u];
-        if(sz[u] > sz[g[v][0]]) {
-            swap(u, g[v][0]);
-        }
+        dfs_sz(u); sz[v] += sz[u];
+        if(sz[u] > sz[g[v][0]]) { swap(u, g[v][0]); }
     }
 }
-
 void dfs_hld(int v = 0) {
     in[v] = t++;
     for(auto u: g[v]) {
